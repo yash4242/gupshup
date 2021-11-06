a9bb96a9eca615f4c41af2ad08227c48dfbf0724
Yash mehan <yash.m@research.iiit.ac.in>
2021-11-06 13:45:58 +0530

    autoscroll and clear the input field

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,12 @@
 const chatFormElem = document.getElementById("chat-form");
+const chatMessagesElem = document.querySelector(".chat-messages");
 const socket = new io();
 
 // socket.emit emits the message, which the below function catches
 socket.on("message", (message) => {
     displayMessage(message);
+    chatMessagesElem.scrollTop = chatMessagesElem.scrollHeight;
+
 });
 
 //what happens on sending a message
@@ -11,6 +14,7 @@ chatFormElem.addEventListener("submit", (e) => {
     e.preventDefault();
     const msgContent = e.target.elements.msg.value; // harvesting the message text
     socket.emit("chatMessage", msgContent); //send the msgContent to server
+    e.target.elements.msg.value = ""; //clearing out the input message field
 });
 
 //once the client receives a `message` how to display the message in DOM
